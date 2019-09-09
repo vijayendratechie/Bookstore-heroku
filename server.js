8199a60926a191c233c4ca2aa12f360dcce934b3
Vijayendra Pagare <vijayendrapagare05@gmail.com>
2019-09-10 02:24:51 +0530

    0.3

--- server.js
+++ server.js
@@ -30,6 +30,17 @@ app.use(bodyparser());
 app.use(express.static(path.join(__dirname,"static")));
 app.use(cookieParser());
 
+app.use((req,res,next) => {
+	res.header("Access-Control-Allow-Origin","*");
+	res.header("Access-Control-Allow-Headers","*");
+	if(req.method === 'OPTIONS')
+	{
+		res.header('Access-Control-Allow-Methods','GET, POST');
+		return res.status(200).json({});
+	}
+	next();
+});
+
 
 app.use(session({
 	secret : 'key',
