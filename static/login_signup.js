c1cdac025a1921d3cbd6ed6c7279b3299a8a2970
Vijayendra Pagare <vijayendrapagare05@gmail.com>
2019-10-01 01:29:12 +0530

    1.5

--- static/login_signup.js
+++ static/login_signup.js
@@ -104,7 +104,7 @@ $(document).ready(function()
 			
 			$.ajax({
 			    type: "POST",
-			    url: "https://discountedtrade.in/login",
+			    url: "https://www.discountedtrade.in/login",
 			    data: {email : email,password : password},
 			    dataType: "json",
 			    success: function(info)
